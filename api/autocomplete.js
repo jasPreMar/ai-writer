304944c2adea81e801bb41718b9acea3a97d8f91
jasPreMar <jasonpmarsh@gmail.com>
2023-11-19 20:33:13 -0600

    Added console logs to the serverless function to test errors.

--- api/autocomplete.js
+++ api/autocomplete.js
@@ -4,24 +4,21 @@ const { OpenAI } = require('openai');
 // Instantiate OpenAI client with your API key
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-  });
+});
 
 module.exports = async (req, res) => {
-  try {
-    // Parse the request body to get the prompt
-    const { prompt } = req.body;
+    console.log('Request body:', req.body);  // Log the incoming request body
 
-    // Call the OpenAI API
-    const completion = await openai.createCompletion({
-      model: 'gpt-3.5-turbo-instruct',
-      prompt: prompt,
-      max_tokens: 20,
-    });
-
-    // Send the response back to the client
-    res.status(200).json(completion.data);
-  } catch (error) {
-    // Handle errors
-    res.status(500).json({ error: error.message });
-  }
+    try {
+        const { prompt } = req.body;
+        const completion = await openai.createCompletion({
+            model: 'gpt-3.5-turbo-instruct',
+            prompt: prompt,
+            max_tokens: 20,
+        });
+        res.status(200).json(completion.data);
+    } catch (error) {
+        console.error('Error:', error);  // Log any errors
+        res.status(500).json({ error: error.message });
+    }
 };
